609a23a33fccbecd3ec9b890f436bfc815dc90d2
Krzysztof Łos <krzysztof.los@vp.pl>
2017-02-15 00:32:50 +0100

    add validation, random picture thermometer, change font, change . to ,

--- Temperature-Converter/Temperature-Converter/Form1.Designer.cs
+++ Temperature-Converter/Temperature-Converter/Form1.Designer.cs
@@ -48,7 +48,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(186, 137);
+            this.button1.Location = new System.Drawing.Point(243, 134);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(99, 34);
             this.button1.TabIndex = 0;
@@ -58,29 +58,32 @@
             // 
             // comboBox1
             // 
+            this.comboBox1.Font = new System.Drawing.Font("DejaVu Serif Condensed", 9.75F, System.Drawing.FontStyle.Bold);
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "skala Celsjusza",
             "skala Fahrenheita",
             "skala Kelvina"});
-            this.comboBox1.Location = new System.Drawing.Point(323, 65);
+            this.comboBox1.Location = new System.Drawing.Point(412, 65);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(151, 24);
+            this.comboBox1.Size = new System.Drawing.Size(196, 23);
             this.comboBox1.TabIndex = 1;
             this.comboBox1.Text = "Wybierz skale";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("DejaVu Serif Condensed", 9.75F, System.Drawing.FontStyle.Bold);
             this.label1.Location = new System.Drawing.Point(19, 65);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(121, 16);
+            this.label1.Size = new System.Drawing.Size(130, 15);
             this.label1.TabIndex = 2;
             this.label1.Text = "Wprowadź wartość";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(185, 65);
+            this.textBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(243, 65);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 3;
@@ -99,7 +102,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("DejaVu Serif Condensed", 9.75F, System.Drawing.FontStyle.Bold);
-            this.label3.Location = new System.Drawing.Point(182, 220);
+            this.label3.Location = new System.Drawing.Point(228, 220);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(126, 15);
             this.label3.TabIndex = 5;
@@ -109,7 +112,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("DejaVu Serif Condensed", 9.75F, System.Drawing.FontStyle.Bold);
-            this.label4.Location = new System.Drawing.Point(362, 220);
+            this.label4.Location = new System.Drawing.Point(483, 220);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(96, 15);
             this.label4.TabIndex = 6;
@@ -118,30 +121,33 @@
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(63, 251);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(0, 16);
+            this.label5.Size = new System.Drawing.Size(0, 15);
             this.label5.TabIndex = 7;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(214, 251);
+            this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(240, 251);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(0, 16);
+            this.label6.Size = new System.Drawing.Size(0, 15);
             this.label6.TabIndex = 8;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(384, 251);
+            this.label7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(483, 251);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(0, 16);
+            this.label7.Size = new System.Drawing.Size(0, 15);
             this.label7.TabIndex = 9;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(40, 314);
+            this.pictureBox1.Location = new System.Drawing.Point(49, 314);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(100, 215);
             this.pictureBox1.TabIndex = 10;
@@ -149,7 +155,7 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(197, 314);
+            this.pictureBox2.Location = new System.Drawing.Point(267, 314);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(100, 215);
             this.pictureBox2.TabIndex = 11;
@@ -157,7 +163,7 @@
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(365, 314);
+            this.pictureBox3.Location = new System.Drawing.Point(508, 314);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(100, 215);
             this.pictureBox3.TabIndex = 12;
@@ -167,7 +173,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(507, 574);
+            this.ClientSize = new System.Drawing.Size(659, 659);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
--- Temperature-Converter/Temperature-Converter/Form1.cs
+++ Temperature-Converter/Temperature-Converter/Form1.cs
@@ -23,62 +23,103 @@ namespace Temperature_Converter
             if (comboBox1.SelectedItem != null && textBox1.Text != "" && textBox1.Text != "")
             {
                 double s_kel, s_fakre, s_cel, dane;
-                dane = Convert.ToDouble(textBox1.Text);
+                string wartosc = textBox1.Text;
+                wartosc = wartosc.Replace(".",",");
+                if (sameliczby(wartosc) == 1)
+                {
+                    dane = Convert.ToDouble(wartosc);
+                    if (comboBox1.SelectedItem == "skala Celsjusza")
+                    {
+                        s_cel = dane;
+                        s_fakre = 32 + 1.8 * s_cel;
+                        s_kel = s_cel + 273.15;
 
-                if(comboBox1.SelectedItem == "skala Celsjusza")
-                {
-                    s_cel = dane;
-                    s_fakre = 32 + 1.8*s_cel;
-                    s_kel = s_cel+273.15;
-                    label5.Text = Convert.ToString(s_cel);
-                    label6.Text = Convert.ToString(s_fakre);
-                    label7.Text = Convert.ToString(s_kel);
-                    
-                }
-                else if(comboBox1.SelectedItem == "skala Fahrenheita")
-                {
-                    s_cel = (dane - 32) /1.8;
-                    s_fakre = dane;
-                    s_kel = s_cel + 273.15;
-                    label5.Text = Convert.ToString(s_cel);
-                    label6.Text = Convert.ToString(s_fakre);
-                    label7.Text = Convert.ToString(s_kel);
+                        s_cel = Math.Round(s_cel, 4);
+                        s_fakre = Math.Round(s_fakre, 4);
+                        s_kel = Math.Round(s_kel, 4);
+
+                        label5.Text = Convert.ToString(s_cel);
+                        label6.Text = Convert.ToString(s_fakre);
+                        label7.Text = Convert.ToString(s_kel);
+
+                    }
+                    else if (comboBox1.SelectedItem == "skala Fahrenheita")
+                    {
+                        s_cel = (dane - 32) / 1.8;
+                        s_fakre = dane;
+                        s_kel = s_cel + 273.15;
+
+                        s_cel = Math.Round(s_cel, 4);
+                        s_fakre = Math.Round(s_fakre, 4);
+                        s_kel = Math.Round(s_kel, 4);
+
+                        label5.Text = Convert.ToString(s_cel);
+                        label6.Text = Convert.ToString(s_fakre);
+                        label7.Text = Convert.ToString(s_kel);
+                    }
+                    else
+                    {
+                        s_cel = dane - 273.15;
+                        s_fakre = 32 + 1.8 * s_cel;
+                        s_kel = dane;
+
+                        s_cel = Math.Round(s_cel, 4);
+                        s_fakre = Math.Round(s_fakre, 4);
+                        s_kel = Math.Round(s_kel, 4);
+
+                        label5.Text = Convert.ToString(s_cel);
+                        label6.Text = Convert.ToString(s_fakre);
+                        label7.Text = Convert.ToString(s_kel);
+                    }
+
+                    Random rand = new Random();
+                    int k1 = rand.Next(3);
+                    int k2 = rand.Next(3);
+                    int k3 = rand.Next(3);
+
+                    if (k1 == 0) pictureBox1.Image = Properties.Resources.projekt_termometru;
+                    else if (k1 == 1) pictureBox1.Image = Properties.Resources.projekt_termometru2;
+                    else pictureBox1.Image = Properties.Resources.projekt_termometru3;
+
+                    if (k2 == 0) pictureBox2.Image = Properties.Resources.projekt_termometru;
+                    else if (k2 == 1) pictureBox2.Image = Properties.Resources.projekt_termometru2;
+                    else pictureBox2.Image = Properties.Resources.projekt_termometru3;
+
+                    if (k3 == 0) pictureBox3.Image = Properties.Resources.projekt_termometru;
+                    else if (k3 == 1) pictureBox3.Image = Properties.Resources.projekt_termometru2;
+                    else pictureBox3.Image = Properties.Resources.projekt_termometru3;
+
                 }
                 else
                 {
-                    s_cel = dane - 273.15;
-                    s_fakre = 32 + 1.8 * s_cel;
-                    s_kel = dane;
-                    label5.Text = Convert.ToString(s_cel);
-                    label6.Text = Convert.ToString(s_fakre);
-                    label7.Text = Convert.ToString(s_kel);
+                    Blad();
                 }
-
-                Random rand = new Random();
-                int k1 = rand.Next(3);
-                int k2 = rand.Next(3);
-                int k3 = rand.Next(3);
-
-                if(k1==0) pictureBox1.Image = Properties.Resources.projekt_termometru;
-                else if(k1==1) pictureBox1.Image = Properties.Resources.projekt_termometru2;
-                else pictureBox1.Image = Properties.Resources.projekt_termometru3;
-
-                if (k2 == 0) pictureBox2.Image = Properties.Resources.projekt_termometru;
-                else if (k2 == 1) pictureBox2.Image = Properties.Resources.projekt_termometru2;
-                else pictureBox2.Image = Properties.Resources.projekt_termometru3;
-
-                if (k3 == 0) pictureBox3.Image = Properties.Resources.projekt_termometru;
-                else if (k3 == 1) pictureBox3.Image = Properties.Resources.projekt_termometru2;
-                else pictureBox3.Image = Properties.Resources.projekt_termometru3;
-
-
 
             }
             else {
-                label5.Text = "Error";
-                label6.Text = "Error";
-                label7.Text = "Error";
+                Blad();
             }
+        }
+
+
+        public int sameliczby(string napis)
+        {
+            for(int i = 0; i < napis.Length; i++)
+            {
+                if(napis[i] == '.' || napis[i] == ',')continue;
+                if (napis[i] < '0' || napis[i] > '9') return 0;
+
+            }
+            return 1;
+        }
+        public void Blad()
+        {
+            label5.Text = "Error";
+            label6.Text = "Error";
+            label7.Text = "Error";
+            pictureBox1.Image = null;
+            pictureBox2.Image = null;
+            pictureBox3.Image = null;
         }
     }
 }
